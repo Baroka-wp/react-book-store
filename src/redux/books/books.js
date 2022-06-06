afb527c121c47c7b4d8e7315c476c9f10b1e50b7
baroka <birotori@gmail.com>
2022-06-06 20:46:23 +0100

    Use Redux in React components

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,7 +4,13 @@ const REMOVE_BOOK = 'REMOVE_BOOK';
 export const addBook = (payload) => ({ type: ADD_BOOK, payload });
 export const deleteBook = (payload) => ({ type: REMOVE_BOOK, payload });
 
-const bookReducer = (state = [], action) => {
+const books = [
+  { id: 1, title: 'Atomic Habits', author: 'James Clear' },
+  { id: 2, title: 'Siddartha', author: 'Hermann Hesse' },
+  { id: 3, title: 'The effective Executive', author: 'Peter Drucker' },
+];
+
+const bookReducer = (state = books, action) => {
   switch (action.type) {
     case REMOVE_BOOK:
       return [...state].filter((s) => s.id !== action.payload);
